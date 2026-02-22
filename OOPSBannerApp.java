5ec80f57d4bf77d32186e155b9f4b0d958bccbd2
sanjukta-byte <guddubiswas1969@gmail.com>
2026-02-22 22:09:39 +0530

    UC3: Refactored banner using String.join() for memory efficiency

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,22 +1,34 @@
 /**
  * OOPSBannerApp
  *
- * UC2: Displays "OOPS" in a 7-line ASCII banner
- * using '*' and spaces.
+ * UC3: Refactored banner creation using String.join()
+ * to improve memory efficiency and readability.
  *
  * @author Sanjukta
- * @version 2.0
+ * @version 3.0
  */
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
+System.out.println(String.join(" ",
+                "  ***** ", "   ***** ", " ******** ", "  ***** "));
 
-        System.out.println(" *****    *****    ******   ****** ");
-        System.out.println("*     *  *     *   *     *  *      ");
-        System.out.println("*     *  *     *   *     *  *      ");
-        System.out.println("*     *  *     *   ******   ****** ");
-        System.out.println("*     *  *     *   *             * ");
-        System.out.println("*     *  *     *   *             * ");
-        System.out.println(" *****    *****    *        ****** ");
+        System.out.println(String.join(" ",
+                "**     **", "**     **", "**     **", "**     **"));
+
+        System.out.println(String.join(" ",
+                "**     **", "**     **", "**     **", "**"));
+
+        System.out.println(String.join(" ",
+                "**     **", "**     **", "******** ", "   **** "));
+
+        System.out.println(String.join(" ",
+                "**     **", "**     **", "**       ", "      **"));
+
+        System.out.println(String.join(" ",
+                "**     **", "**     **", "**       ", "**     **"));
+
+        System.out.println(String.join(" ",
+                "  ***** ", "   ***** ", " **      ", "   ***** "));
     }
 }
